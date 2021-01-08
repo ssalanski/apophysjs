72722740e74b8ba4f59d96eaad32b96751d39abf
Stefan Salanski <stefan.salanski@gmail.com>
2021-01-08 06:58:11 +0000

    push info bar code into a separate function component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
+import React from 'react';
+import InfoBar from './InfoBar.js';
 import './App.css';
 
 function App() {
   // Return the App component.
   return (
     <div className="App">
-	  <div className="InfoBar">info bar...</div>
+	  <InfoBar />
 	  <div className="EditPanel">edit panel...</div>
 	  <div className="PreviewWindow">preview window...</div>
     </div>
---
+++ src/InfoBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './InfoBar.css';
+
+function InfoBar() {
+  // Return the InfoBar component.
+  return (
+    <div className="InfoBar">
+	  info bar component
+    </div>
+  );
+}
+
+export default InfoBar;
