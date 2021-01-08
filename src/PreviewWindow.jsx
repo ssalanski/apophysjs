8644d6124b3bccf13c5f1ad24c96881abfdfa002
Stefan Salanski <stefan.salanski@gmail.com>
2021-01-08 18:55:18 +0000

    using some react hooks, get a reference to the canvas element and add the canvas context to the state
    draw some squares to show its working

--- src/PreviewWindow.jsx
+++ src/PreviewWindow.jsx
@@ -1,11 +1,35 @@
-import React from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import './PreviewWindow.css';
 
 function PreviewWindow() {
+  const canvasRef = useRef(null);
+  const [context, setContext] = useState(null);
+  useEffect( () => {
+    if (canvasRef.current) {
+      // shenanigans to prevent parent container resizing just barely larger than the new canvas size
+      const containerWidth = canvasRef.current.parentElement.offsetWidth;
+      const containerHeight = canvasRef.current.parentElement.offsetHeight;
+      canvasRef.current.parentElement.style.width = containerWidth + 'px';
+      canvasRef.current.parentElement.style.height = containerHeight + 'px';
+      canvasRef.current.width = containerWidth;
+      canvasRef.current.height = containerHeight;
+      
+      const renderCtx = canvasRef.current.getContext('2d');
+      if (renderCtx) {
+        setContext(renderCtx);
+      }
+    }
+
+    // test shapes
+    if(context) context.fillRect(5,5,100,100);
+    if(context) context.fillRect(canvasRef.current.width-5,canvasRef.current.height-5,-100,-100);
+  }, [context]);
+
+
   // Return the PreviewWindow component.
   return (
     <div className="PreviewWindow">
-      <canvas id="previewCanvas"></canvas>
+      <canvas id="previewCanvas" ref={canvasRef}></canvas>
     </div>
   );
 }
